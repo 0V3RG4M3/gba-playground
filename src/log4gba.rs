01e51347b44abdcf822bebbf516c4c624c931c2b
Emmanuel Girardin <emmanuel.girardin@outlook.com>
2024-06-27 00:45:12 +0200

    FIX: add missing log level

--- src/log4gba.rs
+++ src/log4gba.rs
@@ -8,9 +8,6 @@ pub fn log(message_level: MgbaMessageLevel, message: impl core::fmt::Debug) {
     }
 }
 
-pub fn debug(message: impl core::fmt::Debug) {
-    log(MgbaMessageLevel::Debug, message)
-}
 
 pub fn fatal(message: impl core::fmt::Debug) {
     log(MgbaMessageLevel::Fatal, message)
@@ -22,4 +19,12 @@ pub fn error(message: impl core::fmt::Debug) {
 
 pub fn warning(message: impl core::fmt::Debug) {
     log(MgbaMessageLevel::Warning, message)
+}
+
+pub fn debug(message: impl core::fmt::Debug) {
+    log(MgbaMessageLevel::Debug, message)
+}
+
+pub fn info(message: impl core::fmt::Debug) {
+    log(MgbaMessageLevel::Info, message)
 }
