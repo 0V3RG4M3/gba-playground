4022a806e0f0d512a7b0aa97c898624060d7a5f3
Pablo Stebler <pablo@stebler.xyz>
2024-06-28 21:56:48 +0200

    Disable GBA-specific sound code when testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 #![cfg_attr(not(test), no_std)]
 
+#[cfg(not(test))]
 pub mod gba_synth;
+#[cfg(not(test))]
 pub mod log4gba;
 pub mod scene;
+#[cfg(not(test))]
 pub mod static_sounds_lib;
 pub mod tune;
