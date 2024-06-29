4b416118a86d6b9787a2bfc961263ea2d42cb345
Emmanuel Girardin <emmanuel.girardin@outlook.com>
2024-06-29 23:16:17 +0200

    ADD: sfx

---
+++ src/sfx.rs
@@ -0,0 +1,21 @@
+// This file has been automatically generated
+
+pub const ITEM_COLLECTED_STEP_COUNT: u16 = 16;
+pub const ITEM_COLLECTED: [(u8, u8); ITEM_COLLECTED_STEP_COUNT as usize] = [
+    (72, 100),
+    (0, 0),
+    (84, 100),
+    (0, 0),
+    (76, 100),
+    (0, 0),
+    (88, 100),
+    (0, 0),
+    (79, 100),
+    (0, 0),
+    (91, 100),
+    (0, 0),
+    (84, 100),
+    (0, 0),
+    (96, 100),
+    (0, 0),
+];
