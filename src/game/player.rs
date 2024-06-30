ac6d70ac8f189f69d75c3139e6110f6179f092a0
Pablo Stebler <pablo@stebler.xyz>
2024-06-30 12:08:47 +0200

    Adjust the control scheme

--- src/game/player.rs
+++ src/game/player.rs
@@ -34,19 +34,19 @@ impl Player {
         }
 
         let mut cam_yaw_angle = camera.yaw_angle();
-        cam_yaw_angle -= key_input.l() as u8;
-        cam_yaw_angle += key_input.r() as u8;
+        cam_yaw_angle -= key_input.left() as u8;
+        cam_yaw_angle += key_input.right() as u8;
         camera.set_yaw_angle(cam_yaw_angle);
 
         camera.pos.x += camera.yaw_sin() * (key_input.up() as i32);
         camera.pos.x -= camera.yaw_sin() * (key_input.down() as i32);
-        camera.pos.x -= camera.yaw_cos() * (key_input.left() as i32);
-        camera.pos.x += camera.yaw_cos() * (key_input.right() as i32);
+        camera.pos.x -= camera.yaw_cos() * (key_input.l() as i32);
+        camera.pos.x += camera.yaw_cos() * (key_input.r() as i32);
 
         camera.pos.z -= camera.yaw_cos() * (key_input.up() as i32);
         camera.pos.z += camera.yaw_cos() * (key_input.down() as i32);
-        camera.pos.z -= camera.yaw_sin() * (key_input.left() as i32);
-        camera.pos.z += camera.yaw_sin() * (key_input.right() as i32);
+        camera.pos.z -= camera.yaw_sin() * (key_input.l() as i32);
+        camera.pos.z += camera.yaw_sin() * (key_input.r() as i32);
 
         let equipped_item_index = items
             .iter()
