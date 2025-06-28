2c50a8bd2ae849d31f0380ea75c02ebc1a18438e
Manu Gira <emmanuel.girardin@outlook.com>
2025-06-28 16:05:26 +0200

    remote-MOD: automatically enable sound in dummy ROM

--- src/bin/dummy.rs
+++ src/bin/dummy.rs
@@ -4,6 +4,9 @@
 use core::fmt::Write;
 use gba::mgba::{MgbaBufferedLogger, MgbaMessageLevel};
 
+use gba::mmio;
+use gba::sound;
+
 #[panic_handler]
 fn panic_handler(info: &core::panic::PanicInfo) -> ! {
     if let Ok(mut logger) = MgbaBufferedLogger::try_new(MgbaMessageLevel::Fatal) {
@@ -12,7 +15,27 @@ fn panic_handler(info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+fn enable_sound(){
+
+    mmio::SOUND_ENABLED.write(sound::SoundEnable::new().with_enabled(true));
+
+    mmio::LEFT_RIGHT_VOLUME.write(
+        sound::LeftRightVolume::new()
+            .with_right_volume(15)  // attention il semble que le volume doit etre dans [0, 7]
+            .with_left_volume(15)
+            .with_tone1_left(true)
+            .with_tone2_left(true)
+            .with_noise_left(true)
+            .with_tone1_right(true)
+            .with_tone2_right(true)
+            .with_noise_right(true),
+    );
+
+    mmio::SOUND_MIX.write(sound::SoundMix::new().with_psg(sound::PsgMix::_50));
+}
+
 #[no_mangle]
 pub fn main() -> ! {
+    enable_sound();
     loop {}
 }
