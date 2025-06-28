5153e9386e749ddca3461c174e6f878390ede7e7
Manu Gira <emmanuel.girardin@outlook.com>
2025-06-28 17:45:06 +0200

    remote-MOD: simplify ToneFrequency value script

--- scripts/remote/sound_ToneFrequency.js
+++ scripts/remote/sound_ToneFrequency.js
@@ -8,16 +8,11 @@ var ctx = {
   SIZE: 2, // Number of registers used by this object
 }
 
-function ToneFrequency(frequency_rate, stop_when_expired, enabled) {
-  if (frequency_rate < 0 || frequency_rate > 2047) {
+function sendRegData() {
+  if (ctx.frequency_rate < 0 || ctx.frequency_rate > 2047) {
     throw new Error("Frequency must be between 0 and 2047");
   }
-  ctx.frequency_rate = frequency_rate || 0;
-  ctx.stop_when_expired = !!stop_when_expired;
-  ctx.enabled = !!enabled;
-}
 
-function sendRegData() {
   const regData = (ctx.frequency_rate << 0) |
          (ctx.stop_when_expired ? (1 << 14) : 0) |
          (ctx.enabled ? (1 << 15) : 0);
@@ -26,24 +21,9 @@ function sendRegData() {
   outlet(0, "reg_data", ctx.SIZE, regData);
 }
 
-function frequency_rate(value) {
-  log("frequency_rate(): received command:", value);
-  if (value < 0 || value > 2047) {
-    throw new Error("Frequency must be between 0 and 2047");
-  }
-  ctx.frequency_rate = value;
-  sendRegData();
-}
-
-function stop_when_expired(value) {
-  log("stop_when_expired(): received command:", value);
-  ctx.stop_when_expired = !!value;
-  sendRegData();
-}
-
-function enabled(value) {
-  log("enabled(): received command:", value);
-  ctx.enabled = !!value;
+function set_value(key, value){
+  ctx[key] = value;
+  log("set_value(): set", key, "to", value);
   sendRegData();
 }
 
