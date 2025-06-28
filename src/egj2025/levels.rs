7d6107c39e6e091ffa8a1cf2e5f79fc19a55f4dd
Pablo Stebler <pablo@stebler.xyz>
2025-06-28 18:30:56 +0200

    Adapt item positions

--- src/egj2025/levels.rs
+++ src/egj2025/levels.rs
@@ -5,17 +5,17 @@ pub const LEVELS: [fn() -> Level; 2] = [level0, level1];
 
 fn level0() -> Level {
     let mut items: [Option<Item>; 32] = [const { None }; 32];
-    items[0] = Some(Item::new(0, ItemKind::Melon, 96, 2, 8));
-    items[1] = Some(Item::new(1, ItemKind::Watermelon, 34, 2, 15));
+    items[0] = Some(Item::new(0, ItemKind::Melon, 128, 2, 24));
+    items[1] = Some(Item::new(1, ItemKind::Watermelon, 66, 2, 31));
     const PAIRS: [(ItemKind, ItemKind); 1] = [(ItemKind::Melon, ItemKind::Watermelon)];
     Level { items, pairs: &PAIRS }
 }
 
 fn level1() -> Level {
     let mut items: [Option<Item>; 32] = [const { None }; 32];
-    items[0] = Some(Item::new(0, ItemKind::Melon, 96, 2, 8));
-    items[1] = Some(Item::new(1, ItemKind::Watermelon, 34, 2, 15));
-    items[2] = Some(Item::new(2, ItemKind::Watermelon, 0, 2, 24));
+    items[0] = Some(Item::new(0, ItemKind::Melon, 128, 2, 24));
+    items[1] = Some(Item::new(1, ItemKind::Watermelon, 66, 2, 31));
+    items[2] = Some(Item::new(2, ItemKind::Watermelon, 32, 2, 40));
     const PAIRS: [(ItemKind, ItemKind); 1] = [(ItemKind::Watermelon, ItemKind::Watermelon)];
     Level { items, pairs: &PAIRS }
 }
