61111702f8761f887c60d18f67e52501ebefa124
Manu Gira <emmanuel.girardin@outlook.com>
2025-09-18 20:56:24 +0200

    remote-ADD: RegTuneLogReader

--- scripts/remote/pyGBA/reg_tune_logger.py
+++ scripts/remote/pyGBA/reg_tune_logger.py
@@ -1,4 +1,7 @@
 import max4live_udp_cleaner
+from typing import Optional
+import time
+
 
 class IRegTuneLogWriter:
     def newlogfile(self):
@@ -6,6 +9,7 @@ class IRegTuneLogWriter:
 
     def log(self, data: bytes) -> None:
         raise NotImplementedError("log method not implemented")
+
 
 class RegTuneLogWriter(IRegTuneLogWriter):
     def __init__(self, filename: str):
@@ -33,9 +37,40 @@ class RegTuneLogWriter(IRegTuneLogWriter):
             return
 
         if self.is_recording:
-            with open(self.filename, "ab") as file:
-                file.writelines([data])
+            line = repr(data.decode("latin1"))  # latin1 = preserves bytes 0-255
+
+            with open(self.filename, "a", encoding="utf-8") as f:
+                f.write(line + "\n")
+
 
 class RegTuneLogReader:
-    def __init__(self, filename: str):
-        raise NotImplementedError("yet to be implemented")
+    def __init__(self, filename: str, time_scale: Optional[float] = None):
+        self.filename = filename
+        assert time_scale is None or time_scale > 0.0, "time_scale must be positive"
+        self.time_scale: Optional[float] = time_scale
+
+    def read(self):
+        lines: list[bytes] = []
+
+        with open(self.filename, encoding="utf-8") as file:
+            t0 = time.time()
+            for line in file:
+                line = line.strip()
+                data = eval(line).encode()
+                lines.append(data)
+
+        for data in lines:
+            items = data.decode("utf-8").splitlines()
+
+            if len(items) == 0:
+                continue
+
+            if self.time_scale is None:
+                yield data
+                continue
+
+            frame_id = int(items[0])
+            t1 = t0 + (frame_id * self.time_scale / 60.0)
+            to_sleep = max(0.0, t1 - time.time())
+            time.sleep(to_sleep)
+            yield data
