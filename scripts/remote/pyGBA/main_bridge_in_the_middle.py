e7ed79258a6e5a92e57baca4e805f71274c4d2e1
Manu Gira <emmanuel.girardin@outlook.com>
2025-09-18 21:01:10 +0200

    remote-MOD: mock max4live with reg_tune.bin.txt file

--- scripts/remote/pyGBA/main_bridge_in_the_middle.py
+++ scripts/remote/pyGBA/main_bridge_in_the_middle.py
@@ -3,13 +3,13 @@ import time
 import mmio
 from reg_tune_logger import IRegTuneLogWriter, RegTuneLogWriter
 from typing import Optional
-from simple_stream import ISimpleStream, UDPSimpleStream, NullSimpleStream, TCPSimpleStream
+from simple_stream import ISimpleStream, UDPSimpleStream, NullSimpleStream, TCPSimpleStream, FileSimpleStream
+import max4live_udp_cleaner
 
 class BridgeInTheMiddle:
-    FILENAME = "reg_tune.csv"
-    HEADER = "time,cmd,address,value"
 
-    def __init__(self, input_stream: ISimpleStream, output_stream: ISimpleStream, reg_tune_logger:Optional[IRegTuneLogWriter] = None):
+    def __init__(self, input_stream: ISimpleStream, output_stream: ISimpleStream,
+                 reg_tune_logger: Optional[IRegTuneLogWriter] = None):
         self.input_stream: ISimpleStream = input_stream
         self.output_stream: ISimpleStream = output_stream
         self.reg_tune_logger: Optional[IRegTuneLogWriter] = reg_tune_logger
@@ -30,19 +30,27 @@ class BridgeInTheMiddle:
                         if self.reg_tune_logger:
                             self.reg_tune_logger.log(data)
 
+                        data = max4live_udp_cleaner.clean_udp_message(data)
                         output_stream.push(data)
 
 
-
 def main():
-    BridgeInTheMiddle(
-        input_stream=UDPSimpleStream("127.0.0.1", 9999),
-        output_stream=NullSimpleStream(),
-        reg_tune_logger=RegTuneLogWriter("reg_tune4.hex")
-    ).run()
+    if False:
+        BridgeInTheMiddle(
+            input_stream=UDPSimpleStream("127.0.0.1", 9999),
+            output_stream=NullSimpleStream(),
+            reg_tune_logger=RegTuneLogWriter("reg_tune.bin.txt")
+        ).run()
+    else:
+        BridgeInTheMiddle(
+            input_stream=FileSimpleStream("reg_tune.csv.bin.txt"),
+            output_stream=TCPSimpleStream("localhost", 8888),
+        ).run()
+
     return
     time.sleep(0.1)
-    generate_reg_tune_rs.main_tune()
+    generate_reg_tune_rs.main_tune()  # TODO: update generator to new files
+
 
 if __name__ == "__main__":
     main()
