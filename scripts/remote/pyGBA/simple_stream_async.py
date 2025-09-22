eac1d95c54480dd60d025e668eef29d826fd6777
Manu Gira <emmanuel.girardin@outlook.com>
2025-09-22 18:42:06 +0200

    remote-FIX: UDPSimpleStreamAsync

--- scripts/remote/pyGBA/simple_stream_async.py
+++ scripts/remote/pyGBA/simple_stream_async.py
@@ -1,11 +1,12 @@
 import asyncio
+from typing import Optional
 
 
 class ISimpleStreamAsync:
-    def push(self, data: bytes) -> None:
+    async def push(self, data: bytes) -> None:
         raise NotImplementedError()
 
-    def read(self) -> bytes:
+    async def read(self) -> bytes:
         raise NotImplementedError()
 
     def __aenter__(self):
@@ -51,37 +52,52 @@ class TCPSimpleStreamAsync(ISimpleStreamAsync):
             return data
         return b''
 
+class UDPProtocol(asyncio.DatagramProtocol):
+    def __init__(self, queue: asyncio.Queue):
+        self.queue = queue
+
+    def datagram_received(self, data: bytes, addr):
+        self.queue.put_nowait((data, addr))
+
+    def error_received(self, exc):
+        print(f"Error received: {exc}")
 
 # TODO: test this class
 class UDPSimpleStreamAsync(ISimpleStreamAsync):
     def __init__(self, host: str, port: int):
         self.host = host
         self.port = port
-        self.transport = None
-        self.protocol = None
+
+
+        self._transport: Optional[asyncio.DatagramTransport] = None
         self._is_opened = False
+        self._protocol: UDPProtocol = UDPProtocol(queue=asyncio.Queue())
+
 
     def __enter__(self):
         raise NotImplementedError("Use 'async with' statement to open/close the stream.")
 
     async def __aenter__(self):
         loop = asyncio.get_running_loop()
-        self.transport, self.protocol = await loop.create_datagram_endpoint(
-            lambda: asyncio.DatagramProtocol(),
+        self._transport, protocol = await loop.create_datagram_endpoint(
+            lambda: self._protocol,
             local_addr=(self.host, self.port)
         )
         self._is_opened = True
         return self
 
     async def __aexit__(self, exc_type, exc_value, traceback):
-        if self.transport:
-            self.transport.close()
+        if self._transport:
+            self._transport.close()
         self._is_opened = False
 
     async def push(self, data: bytes) -> None:
         assert self._is_opened, "Stream is not opened. Use 'async with' statement to open the stream."
-        if self.transport:
-            self.transport.sendto(data)
+        if self._transport:
+            self._transport.sendto(data)
 
     async def read(self) -> bytes:
-        raise NotImplementedError("UDP read is not implemented in this example.")
+        assert self._is_opened, "Stream is not opened. Use 'async with' statement to open the stream."
+        data, addr = await self._protocol.queue.get()
+        return data
+
