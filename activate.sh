a8066d482c6bb131e74a64c8e5b5867e8c6ef285
Manu Gira <emmanuel.girardin@outlook.com>
2026-06-19 19:36:53 +0200

    env-FIX: remove files dependent of local environment

--- activate.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Source .env and add MGBA_PATH directory to PATH
-if [ -f .env ]; then
-    source .env
-    export PATH="$(dirname "$MGBA_PATH"):$PATH"
-    echo "Added $(dirname "$MGBA_PATH") to PATH."
-else
-    echo ".env file not found."
-fi
