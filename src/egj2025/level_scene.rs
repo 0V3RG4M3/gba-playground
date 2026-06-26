3267adebe15c008f4b70b0cef3fa8db90209c9ba
Manu Gira <emmanuel.girardin@outlook.com>
2026-06-26 19:37:06 +0200

    gba_synth2-REF: isolate reg_tune from gba_synth2. gba_synth2 now takes a tune as parameter

--- src/egj2025/level_scene.rs
+++ src/egj2025/level_scene.rs
@@ -13,6 +13,7 @@ use crate::egj2025::end_scene::EndScene;
 use crate::egj2025::event_scene::EventScene;
 use crate::egj2025::level::Level;
 use crate::egj2025::level_scene_runners;
+use crate::egj2025::reg_tune;
 use crate::egj2025::sprites;
 use crate::fixed::Fixed;
 use crate::gba_synth2;
@@ -33,7 +34,7 @@ impl<L: Level> LevelScene<L> {
         camera.pos.z = Fixed::from_int(128);
         camera.set_pitch_angle(16);
 
-        gba_synth2::init_synth();
+        gba_synth2::init_synth(&reg_tune::TUNE_TRACK1, reg_tune::TUNE_SIZE, reg_tune::TUNE_LOOP_SIZE);
 
         loop {
             bios::VBlankIntrWait();
--- src/gba_synth2.rs
+++ src/gba_synth2.rs
@@ -2,16 +2,14 @@ use gba::mmio;
 use gba::mmio::{Safe, VolAddress};
 use gba::sound::{LeftRightVolume, PsgMix, SoundEnable, SoundMix};
 
-use crate::reg_tune;
-
 static mut LOOP_DELTA_COUNT: i8 = 0;
 static mut CURRENT_TIME_STEP_TUNE_1: u16 = 0;
 static mut CURRENT_INDEX_TUNE_1: usize = 0; // frame step on tune 1
-#[unsafe(link_section = ".ewram")]
-static mut CURRENT_TUNE_1: [(u16, u8, u32, u32); reg_tune::TUNE_SIZE as usize] =
-    [(0, 0, 0, 0); reg_tune::TUNE_SIZE as usize];
+static mut CURRENT_TUNE_1: *const (u16, u8, u32, u32) = core::ptr::null();
+static mut CURRENT_TUNE_SIZE: usize = 0;
+static mut CURRENT_TUNE_LOOP_SIZE: u16 = 0;
 
-pub fn init_synth() {
+pub fn init_synth(tune: &'static [(u16, u8, u32, u32)], tune_size: u16, tune_loop_size: u16) {
     mmio::SOUND_ENABLED.write(SoundEnable::new().with_enabled(true));
 
     mmio::LEFT_RIGHT_VOLUME.write(
@@ -28,7 +26,14 @@ pub fn init_synth() {
 
     mmio::SOUND_MIX.write(SoundMix::new().with_psg(PsgMix::_50));
 
-    play_tune(reg_tune::TUNE_TRACK1);
+    unsafe {
+        CURRENT_TUNE_1 = tune.as_ptr();
+        CURRENT_TUNE_SIZE = tune_size as usize;
+        CURRENT_TUNE_LOOP_SIZE = tune_loop_size;
+        CURRENT_INDEX_TUNE_1 = 0;
+        CURRENT_TIME_STEP_TUNE_1 = 0;
+        LOOP_DELTA_COUNT = 0;
+    }
 }
 
 pub fn write(size: u8, addr: u32, value: u32) {
@@ -42,20 +47,11 @@ pub fn write(size: u8, addr: u32, value: u32) {
     }
 }
 
-pub fn play_tune(reg_tune1: [(u16, u8, u32, u32); reg_tune::TUNE_SIZE as usize]) {
-    unsafe {
-        CURRENT_TUNE_1 = reg_tune1;
-    }
-}
-
 pub fn play_step() {
     unsafe {
+        let tune = core::slice::from_raw_parts(CURRENT_TUNE_1, CURRENT_TUNE_SIZE);
         loop {
-            //if LOOP_DELTA_COUNT < 0 || LOOP_DELTA_COUNT > 1{
-            //    throw "Invalid LOOP_DELTA_COUNT value";
-            //}
-
-            let (next_time_step, size, addr, value) = CURRENT_TUNE_1[CURRENT_INDEX_TUNE_1]; // time step
+            let (next_time_step, size, addr, value) = tune[CURRENT_INDEX_TUNE_1]; // time step
             let wait_on_loop_back = LOOP_DELTA_COUNT != 0;
             if (next_time_step > CURRENT_TIME_STEP_TUNE_1) || (wait_on_loop_back) {
                 break;
@@ -64,14 +60,14 @@ pub fn play_step() {
             // write the value to the address
             write(size, addr, value);
 
-            CURRENT_INDEX_TUNE_1 = (CURRENT_INDEX_TUNE_1 + 1) % reg_tune::TUNE_SIZE as usize;
+            CURRENT_INDEX_TUNE_1 = (CURRENT_INDEX_TUNE_1 + 1) % CURRENT_TUNE_SIZE;
             if CURRENT_INDEX_TUNE_1 == 0 {
                 LOOP_DELTA_COUNT += 1;
             }
         }
 
         // update time steps
-        CURRENT_TIME_STEP_TUNE_1 = (CURRENT_TIME_STEP_TUNE_1 + 1) % reg_tune::TUNE_LOOP_SIZE;
+        CURRENT_TIME_STEP_TUNE_1 = (CURRENT_TIME_STEP_TUNE_1 + 1) % CURRENT_TUNE_LOOP_SIZE;
         if CURRENT_TIME_STEP_TUNE_1 == 0 {
             LOOP_DELTA_COUNT -= 1;
         }
