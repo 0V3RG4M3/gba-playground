7ec2385e59b7043dc00960a481068f618abbe832
Pablo Stebler <pablo@stebler.xyz>
2026-07-03 23:22:50 +0200

    Introduce SIO

--- src/egj2026/screen_splash_scene.rs
+++ src/egj2026/screen_splash_scene.rs
@@ -1,3 +1,5 @@
+use core::fmt::Write;
+
 use gba::interrupts::IrqBits;
 use gba::mmio::DISPCNT;
 use gba::prelude::{DisplayControl, DisplayStatus, VideoMode};
@@ -11,8 +13,40 @@ pub struct ScreenSplashScene {}
 
 impl ScreenSplashScene {
     fn wait_start_bt() {
+        let mut rcnt = mmio::RCNT.read();
+        rcnt &= !(1 << 14);
+        rcnt &= !(1 << 15);
+        mmio::RCNT.write(rcnt);
+
+        let mut siocnt = mmio::SIOCNT.read();
+        siocnt &= !(1 << 12);
+        siocnt |= 1 << 13;
+        siocnt |= 1 << 14;
+        mmio::SIOCNT.write(siocnt);
+
         loop {
             bios::VBlankIntrWait();
+            let mut siocnt = mmio::SIOCNT.read();
+            let parent = (siocnt >> 2) & 1 == 0;
+            let ready = (siocnt >> 3) & 1 != 0;
+            if ready {
+                if let Ok(mut logger) =
+                    gba::mgba::MgbaBufferedLogger::try_new(gba::mgba::MgbaMessageLevel::Debug)
+                {
+                    writeln!(logger, "{ready} {parent} {siocnt:x?}").ok();
+                }
+
+                if parent {
+                    mmio::SIOMLT_SEND.write(42);
+                    siocnt |= 1 << 7;
+                    mmio::SIOCNT.write(siocnt);
+                } else {
+                    mmio::SIOMLT_SEND.write(24);
+                }
+            }
+
+            gba::RUST_IRQ_HANDLER.write(Some(irq_handler));
+
             let key_input = mmio::KEYINPUT.read();
             if key_input.start() {
                 break;
@@ -30,7 +64,7 @@ impl Scene for ScreenSplashScene {
 
     fn run(&mut self, _: &mut Self::C) -> SceneRunner<Self::C> {
         mmio::DISPSTAT.write(DisplayStatus::new().with_irq_vblank(true));
-        mmio::IE.write(IrqBits::new().with_vblank(true).with_hblank(true));
+        mmio::IE.write(IrqBits::new().with_vblank(true).with_hblank(true).with_serial(true));
         mmio::IME.write(true);
 
         video::video3_set_bitmap(&screens::SCREEN_SPLASH);
@@ -41,5 +75,23 @@ impl Scene for ScreenSplashScene {
         log4gba::debug("start bt pressed");
 
         loop {}
+    }
+}
+
+#[unsafe(link_section = ".iwram")]
+extern "C" fn irq_handler(irq_bits: IrqBits) {
+    if !irq_bits.serial() {
+        return;
+    }
+
+    let siocnt = mmio::SIOCNT.read();
+    let parent = (siocnt >> 2) & 1 == 0;
+    let ready = (siocnt >> 3) & 1 != 0;
+    let siomulti0 = mmio::SIOMULTI0.read();
+    let siomulti1 = mmio::SIOMULTI1.read();
+    if let Ok(mut logger) =
+        gba::mgba::MgbaBufferedLogger::try_new(gba::mgba::MgbaMessageLevel::Debug)
+    {
+        writeln!(logger, "irq {ready} {parent} {siocnt:x?} {siomulti0} {siomulti1}").ok();
     }
 }
